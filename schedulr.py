f90ec7798b90b7f976e81472196771ec7909cb6a
Doug <douglas.moyer42@gmail.com>
2018-01-27 13:22:45 -0500

    Working login system (if not secure)

--- schedulr.py
+++ schedulr.py
@@ -1,19 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('schedulr.db')
+def login(username, password):
+    conn = sqlite3.connect('schedulr.db')
 
-loggedIn = False
-userID = 0
-
-def login(username = None, password = None):
-    global loggedIn, userID
-    if loggedIn:
-        print("You already logged in")
-        return False
-    if username is None:
-        username = input("Username: ")
-    if password is None:
-        password = input("Password: ")
     c = conn.cursor()
     c.execute('SELECT * FROM main.tusers WHERE Username = ? AND Password = ?',
         (username, password))
@@ -24,21 +13,19 @@ def login(username = None, password = None):
         print("Login Failed")
         return False
     
-    loggedIn = True
     userID = userrow[0]
-
     return True
 
-def signup():
-    username = input("What username would you like to use? ")
-    password = input("Input a password: ")
+def signup(username, password):
+    conn = sqlite3.connect('schedulr.db')
+
     c = conn.cursor()
     c.execute('INSERT INTO main.tusers (Username, Password) values (?, ?)',
         (username, password))
     conn.commit()
     c.close()
 
-    return login(username = username, password = password)
+    return login(username, password)
 
 def scheduleBlock():
     return False
@@ -50,26 +37,9 @@ def checkAvailable():
     return False
 
 def showUsers():
+    conn = sqlite3.connect('schedulr.db')
+
     c = conn.cursor()
     c.execute('SELECT * FROM main.tusers')
     
     return False
-
-if __name__ == "__main__":
-    command = ''
-    while command != 'q':
-        command = input('What would you like to do? ')
-        if command == 'h':
-            print('Help Menu')
-        elif command == 's':
-            signup()
-        elif command == 'l':
-            login()
-        elif command == 'f':
-            addFriend()
-        elif command == 'c':
-            scheduleBlock()
-        elif command == 'u':
-            showUsers()
-        elif command == '?':
-            checkAvailable()
--- server.py
+++ server.py
@@ -1,26 +1,57 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
 import schedulr
+
+# App setup
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret tunnel'
+)
+
+# Login Manager Setup
+# Perhaps we move the user class to a different file
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.name = username + "ABC"
+        self.id = username
+
+@login_manager.user_loader
+def load_user(username):
+    return User(username)
+
+# App endpoints
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        True # do_the_login()
-        print("Posted")
-    else:
-        False # show_the_login_form()
+        username = request.form['username']
+        password = request.form['password']
+        if schedulr.login(username, password):
+            login_user(User(username))
+            return redirect(url_for('index'))
     return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-    else:
-        False
-    return 'Register Page'
+        if schedulr.signup(username, password):
+            login_user(User(username))
+            return redirect(url_for('index'))
+    return render_template('register.html')
 
 @app.route('/schedule', methods=['GET', 'POST'])
+@login_required
 def schedule():
     if request.method == 'POST':
         True
